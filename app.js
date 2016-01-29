92e2c24c01498301c7c4b61eb7ad721edbbd5094
nikonthegreat <nikolay.brel@gmail.com>
2016-01-30 00:47:57 +0300

    calculator inside features

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
 var calc = new Calculator({
-        resultField: document.querySelector('.result-text'),
-
-        button1: document.querySelector('.button1'),
-        button2: document.querySelector('.button2'),
-        button3: document.querySelector('.button3'),
-        button4: document.querySelector('.button4'),
-        button5: document.querySelector('.button5'),
-        button6: document.querySelector('.button6'),
-        button7: document.querySelector('.button7'),
-        button8: document.querySelector('.button8'),
-        button9: document.querySelector('.button9'),
-
-        buttonDivide: document.querySelector('.button-divide'),
-        buttonMultiply: document.querySelector('.button-multiply'),
-        buttonSub: document.querySelector('.button-sub'),
-        buttonSum: document.querySelector('.button-sum'),
-
-        buttonDivide: document.querySelector('.button-divide'),
-        buttonMultiply: document.querySelector('.button-multiply'),
-        buttonSub: document.querySelector('.button-sub'),
-        buttonSum: document.querySelector('.button-sum'),
-
-        buttonComma: document.querySelector('.button-comma'),
-        buttonErase: document.querySelector('.button-erase'),
-        buttonClear: document.querySelector('.button-clear'),
-        buttonResult: document.querySelector('.button-result')
+        element: document.getElementById('calculator')
 });
